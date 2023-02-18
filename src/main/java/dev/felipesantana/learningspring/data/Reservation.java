cecb304166c3d7656761788e72b619b8b5cdc818
Felipe Santana <felipesantanaalbuquerque9@gmail.com>
2023-02-18 09:07:00 -0300

    :sparkles:new: added getters and setters

--- src/main/java/dev/felipesantana/learningspring/data/Reservation.java
+++ src/main/java/dev/felipesantana/learningspring/data/Reservation.java
@@ -20,4 +20,46 @@ public class Reservation {
 
     @Column(name = "RES_DATE")
     private Date resDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(String guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getResDate() {
+        return resDate;
+    }
+
+    public void setResDate(Date resDate) {
+        this.resDate = resDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", roomId='" + roomId + '\'' +
+                ", guestId='" + guestId + '\'' +
+                ", resDate=" + resDate +
+                '}';
+    }
 }
